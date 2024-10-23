49fa5af41a9aaeaf48e35ec0f8f84c94d701825e
ksu_kotovaa <ksu_kotovaa@mail.ru>
2024-10-24 00:44:12 +0300

    finish index

--- IGI/LR5/Lab5/carshowroom/settings.py
+++ IGI/LR5/Lab5/carshowroom/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -127,3 +127,7 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = '/employee_photos/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'employee_photos')
+STATIC_URL = 'catalog/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "catalog/static",
+    ]
